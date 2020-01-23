1a8ce76590e914bad4984ebf9692c24310e1ce1d
ZHI XUAN MO <zhixuanmo@ZHIdeMacBook-Air.local>
2020-01-23 16:58:47 -0500

    hello

---
+++ destroytheworld/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  destroytheworld
+//
+//  Created by ZHI XUAN MO on 1/23/20.
+//  Copyright © 2020 zhixuan mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello")
+    }
+
+
+}
+
